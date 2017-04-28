369c903bd229762734473aa6374b7518e483b0c9
Mark Burrell (ORF) <markrb060@gmail.com>
2017-04-28 12:51:26 -0700

    removed obsolete class and fixed mistakes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeachExperiment.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeachExperiment.java
@@ -5,6 +5,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 
 /**
@@ -13,10 +14,14 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name = "MRBDriveCode", group = "exercises")
 public class TeachExperiment extends LinearOpMode {
     DcMotor leftMotor, rightMotor;
-    public void runOpMode()
+    public void runOpMode() throws InterruptedException
     {
+
         leftMotor = hardwareMap.dcMotor.get("left_motor");
         rightMotor = hardwareMap.dcMotor.get("right_motor");
+        rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
         while (opModeIsActive()) {
             leftMotor.setPower(gamepad1.left_stick_y);
             rightMotor.setPower(gamepad1.right_stick_y);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetest.java
+++
@@ -1,7 +0,0 @@
-
-
-/**
- * Created by Mark on 3/10/2017.
- */
-public class drivetest  {
-}
